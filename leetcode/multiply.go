b01a19bb6ce1d23dd15641e44be9e25f77244a4f
hore <chenhao@kedouinc.com>
2019-08-01 13:16:06 +0800

    feat: 修改字符相乘算法

--- leetcode/multiply.go
+++ leetcode/multiply.go
@@ -2,82 +2,73 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
 func main() {
-	fmt.Println(addStr("999", "998"))
+	fmt.Println(multiply("98765432", "23456789"))
 }
 
 /**
- *  字符串相乘。
+ * ========== 字符串相乘问题（大数乘法） ==========
  * 以字符串的形式表示的非负整数 num1 和 num2，计算两者的乘积用字符串表示。
+ *
+ * ========== 解题思路 ==========
+ * 乘法即每一项相乘，然后相加。即： 455 * 34 = 34 * 400 + 34 * 50 + 34 * 5 = 15470
+ * 详见：大数相乘 http://www2.lssh.tp.edu.tw/~hlf/class-1/lang-c/big_num3.htm
  */
 func multiply(num1 string, num2 string) string {
 	if num1 == "0" || num2 == "0" {
 		return "0"
 	}
+
+	num1 = Reverse(num1)
+	num2 = Reverse(num2)
+
 	len1 := len(num1)
 	len2 := len(num2)
 
-	var nums string
+	var arr = make([]int, len1 + len2)
 
 	for i := 0; i < len1; i++ {
 		for j := 0; j < len2; j++ {
-			pos1 := len1 - 1 - i
-			pos2 := len2 - 1 - j
-			n1, _ := strconv.Atoi(string(num1[i]))
-			n2, _ := strconv.Atoi(string(num2[j]))
-			st := strconv.Itoa(n1 * n2)
-			if nums != "" {
-				nums = addStr(nums, paddingZero(st, pos1 + pos2))
-			} else {
-				nums = paddingZero(st, pos1 + pos2)
-			}
+
+			// ASCII 码计算差值
+			n1 := num1[i] - '0'
+			n2 := num2[j] - '0'
+
+			arr[i + j] += int(n1) * int(n2)
 		}
 	}
 
-	return nums
-}
+	fmt.Println(arr)
 
-func paddingZero(str string, num int) string {
-	for i := 0; i < num; i ++ {
-		str = str + "0"
-	}
-
-	return str
-}
-// 实现大数相加
-func addStr(num1 string, num2 string) string {
-	curry := 0
-	len1 := len(num1)
-	len2 := len(num2)
-	digit := 1
 	rtn := ""
+	var curry int = 0
 
-	for digit <= len1 || digit <= len2 {
-		var d1 int
-		var d2 int
-		if len1 < digit {
-			d1 = 0
-		} else {
-			d1, _ = strconv.Atoi(string(num1[len1 - digit]))
-		}
-		if len2 < digit {
-			d2 = 0
-		} else {
-			d2, _ = strconv.Atoi(string(num2[len2 - digit]))
-		}
-		multi := d1 + d2
-		rtn = strconv.Itoa((multi + curry) % 10) + rtn
+	for k := 0; k < len(arr) - 1; k ++ {
 
-		curry = (multi + curry) / 10
-		digit ++
+		value := arr[k] + curry
+
+		if value  > 9 {
+			curry = value / 10
+
+			rtn += string(value - curry * 10 + '0')
+		} else {
+			curry = 0
+			rtn += string(value + '0')
+		}
 	}
 	if curry != 0 {
-		rtn = strconv.Itoa(curry) + rtn
+		rtn =  rtn + string(curry + '0')
 	}
 
-	return rtn
+	return Reverse(rtn)
 }
 
+func Reverse(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
