e268d49311a1504f4ee6686ca6ccec04c18f90f0
hore <chenhao@kedouinc.com>
2019-08-06 15:13:49 +0800

    feat: 串联字符串查询优化，解题思路

--- leetcode/findSerialSubString.go
+++ leetcode/findSerialSubString.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(findSerialSubString("aaaa", []string{"a","a"}))
+	fmt.Println(findSerialSubString("barfoothefoobarman", []string{"foo","bar"}))
 }
 
 
@@ -12,69 +12,59 @@ func main() {
  * 给定一个字符串 s 和长度一直的单词 words，找出 s 中恰好由 words 所有单词串联成的子串起始位置。words 串联顺序不固定。
  *
  * ========== 问题解决思路 ===========
- *
+ * 1. 遍历字符串中的每个子串长度的字符，判断子串是否符合条件即 0 -> (sLen - subSLen) 中每个 subSLen 字符串的是否符合条件
+ * 2. 判断子串是否符合条件。先用 maps 存储 words 的每个单词出现的次数比如 maps['foo'] => 1，记录每个单词长度 length
+ * 3. 循环 words 的长度，依次在 maps 查询子串中的每个单词。仅当每个单词都出先且出现的次数与 maps 完全一致时，子串符合标准。
  */
-
-type Word struct {
-	num int
-	pos []int
-}
 func findSerialSubString(s string, words []string) []int {
-	if words == nil || len(words) == 0 {
-		return nil
+	if words == nil {
+		return []int{}
+	}
+	nums := len(words)
+
+	if nums == 0 {
+		return []int{}
+	}
+	length := len(words[0])
+
+	if length == 0 {
+		return []int{}
+	}
+	maps := make(map[string]int)
+	substringLen := length * nums
+
+	// 如果长度不够
+	if len(s) < substringLen {
+		return []int{}
 	}
 
-	wordsMaps := make(map[string]int)
-	initialMaps := make(map[string]int)
-	wordLen := len(words[0])
-	substringLen := wordLen * len(words)
-
-	for i := 0; i < len(words); i++ {
-		initialMaps[words[i]] ++
-		wordsMaps[words[i]]++
+	for _, str := range words {
+		maps[str] += 1
 	}
 
-	i := 0
-	pos := 0
-	var rtn []int
-	match := false
-	for i <= len(s) - wordLen {
-		if !match && i + substringLen > len(s) {
-			return rtn
-		}
-		str := s[i:i + wordLen]
-		if wordsMaps[str] > 0 {
-			if !match {
-				match = true
-				pos = i
-			}
-			wordsMaps[str] --
-			i = i + wordLen
+	rtn := make([]int, 0, 30)
 
-			if i - pos == substringLen {
-				rtn = append(rtn, pos)
-				match = false
-				i = pos + 1
-
-				for k, v := range initialMaps {
-					wordsMaps[k] = v
-				}
-			}
-		} else {
-			if match {
-				match = false
-				i = pos + 1
-			} else {
-				i++
-			}
-
-			pos = 0
-
-			for k, v := range initialMaps {
-				wordsMaps[k] = v
-			}
-		}
+	for i := 0; i <= len(s) - substringLen; i++ {
+		isSub(s[i:(i + substringLen)], maps, length, nums, i, &rtn)
 	}
 
 	return rtn
+}
+
+func isSub(s string, maps map[string]int, length int, nums int, pos int, rtn *[]int) {
+	find := make(map[string]int)
+
+	for i := 0; i < nums; i ++ {
+		str := s[i * length:(i + 1) * length]
+		record, ok := maps[str]
+		fRecord, fOk := find[str]
+
+		if !ok || (fOk && fRecord >= record) {
+			return
+		} else {
+			find[str] ++
+		}
+	}
+
+	*rtn = append(*rtn, pos)
 }
