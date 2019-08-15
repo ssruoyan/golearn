adeff8a03914b9e6813134391237f6a08d1ff2e9
hore <chenhao@kedouinc.com>
2019-08-15 10:55:53 +0800

    feat: 初始值使用内置的最小整形

--- leetcode/maxProduct.go
+++ leetcode/maxProduct.go
@@ -3,17 +3,18 @@ package main
 import (
 	"fmt"
 	"github.com/ssruoyan/golearn/leetcode/tool"
+	"golang.org/x/tools/container/intsets"
 )
 
 func main() {
-	fmt.Println(maxProduct([]int{2,3,-2,4,9}))
+	fmt.Println(maxProduct([]int{-2}))
 }
 
 /**
  * ========== 最大乘积问题 ==========
  */
 func maxProduct(nums []int) int {
-	imax, imin, max := 1, 1, 0
+	imax, imin, max := 1, 1, intsets.MinInt
 	for _, v := range nums {
 		if v < 0 {
 			imax, imin = imin, imax
